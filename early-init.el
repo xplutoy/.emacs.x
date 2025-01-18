79b9348ffb7f2ccb2b8d6fd2c9a598e9afb2ba1f
yx <yangxue.cs@foxmail.com>
2025-01-18 17:01:48 +0800

    chore: clean

--- early-init.el
+++ early-init.el
@@ -13,20 +13,20 @@
 (setopt debug-on-error t)
 (setopt gc-cons-threshold most-positive-fixnum)
 
+(add-to-list 'default-frame-alist '(font . "Iosevka-15"))
+
 (add-to-list 'default-frame-alist '(width  . 80))
 (add-to-list 'default-frame-alist '(height . 40))
 (add-to-list 'default-frame-alist '(menu-bar-lines . nil))
 (add-to-list 'default-frame-alist '(tool-bar-lines . nil))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
 
-(add-to-list 'default-frame-alist '(font . "Iosevka-15"))
-
 (add-hook 'after-init-hook (lambda ()
 			     (setopt debug-on-error nil)
 			     (setopt gc-cons-threshold (* 16 1024 1024))))
 
-(advice-add #'display-startup-screen :override #'ignore)
 (advice-add #'display-startup-echo-area-message :override #'ignore)
 
-(provide 'early-init)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; early-init.el ends here
