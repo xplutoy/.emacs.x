2b43a63fb54ca2f93e53b80a489e4ac0b419a942
yx <yangxue.cs@foxmail.com>
2025-02-14 21:53:59 +0800

    chore: clean feeds

--- extras/init-misc.el
+++ extras/init-misc.el
@@ -38,18 +38,14 @@
   :bind (("C-c <return>" . gptel-send)
 	 ("C-c C-<return>" . gptel-menu))
   :custom
-  (gptel-model 'moonshot-v1-32k)
-  (gptel-backend (gptel-make-openai "kimi"
-		   :host "api.moonshot.cn"
+  (gptel-model 'deepseek-chat)
+  (gptel-backend (gptel-make-openai "DeepSeek"
+		   :host "api.deepseek.com"
 		   :key 'gptel-api-key
-		   :models '(moonshot-v1-32k)))
+		   :endpoint "/chat/completions"
+		   :models '(deepseek-chat deepseek-coder)))
   (gptel-default-mode 'org-mode)
   :config
-  (gptel-make-openai "DeepSeek"
-    :host "api.deepseek.com"
-    :key 'gptel-api-key
-    :endpoint "/chat/completions"
-    :models '(deepseek-chat deepseek-coder))
   (when IS-WIN (setopt gptel-use-curl nil))
   (add-hook 'gptel-post-response-functions 'gptel-end-of-response))
 
@@ -96,7 +92,6 @@
   :custom
   (elfeed-feeds
    '(("https://planet.emacslife.com/atom.xml" emacs)
-     ("http://xahlee.info/emacs/emacs/blog.xml" emacs)
      ("https://www.juliabloggers.com/feed/" julia)
      ("https://feeds.feedburner.com/RBloggers" R data-sci)
      ("https://python-bloggers.com/feed/" data-sci python)
@@ -104,21 +99,13 @@
      ("https://planet.lisp.org/rss20.xml" lisp)
      ("https://planet.scheme.org/atom.xml" scheme)
      ("https://planet.haskell.org/rss20.xml" haskell)
-     ("https://roife.github.io/index.xml" pl)
-     ("https://www.cyberciti.biz/feed/" linux)
-     ("https://eranraviv.com/feed/" math)
      ("https://spaces.ac.cn/feed" math ai-new)
      ("https://writings.stephenwolfram.com/feed/" math)
      ("https://www.kdnuggets.com/feed" data-sci)
      ("https://towardsdatascience.com/feed" data-sci)
      ("https://lilianweng.github.io/index.xml" ai-new)
-     ("https://machinelearningmastery.com/feed/" ai-new)
      ("https://www.marktechpost.com/feed/" ai-new)
-     ("https://deepmind.google/blog/rss.xml" ai-new)
-     ("https://bair.berkeley.edu/blog/feed.xml" ai-new)
-     ("https://ai.stanford.edu/blog/feed.xml" ai-new)
      ("https://rss.arxiv.org/rss/cs.AI+cs.CV+cs.LG" ai-arxiv)
-     ("https://simonwillison.net/atom/everything/" ai-blog)
      ("https://wangyurui.com/feed.xml" life)
      ("https://www.ruanyifeng.com/blog/atom.xml" tech)))
   :config
