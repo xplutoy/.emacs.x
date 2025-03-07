cd738e23d41c794682b076b8ce5468f2f2a5611c
yx <yangxue.cs@foxmail.com>
2025-03-07 14:40:05 +0800

    feat: add user-local.el

---
+++ user-local.el
@@ -0,0 +1,22 @@
+;;; -*- lexical-binding: t -*-
+
+;; Author:  xplutoyz
+;; Created: 2025-03-07 14:37:30
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+;; (require 'init-color-rg)
+
+;; (require 'init-dwim-shell-command)
+
+;; (require 'init-ultra-scroll)
+
+;;
+
+
+;;; user-local.el ends here
